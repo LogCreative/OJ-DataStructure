d2c5a2fd8b9482f22719ef2c081881f15d65ef68
LogCreative <logcreative@outlook.com>
2025-09-21 15:56:01 +0800

    pass 22 py

---
+++ Leetcode/22.括号生成.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode.cn id=22 lang=python3
+#
+# [22] 括号生成
+#
+
+from typing import List
+
+# @lc code=start
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        ans = []
+
+        def backtrack(S: List[str], left, right):
+            if len(S) == 2 * n:
+                ans.append(''.join(S))
+                return
+            
+            if left < n:
+                S.append('(')
+                backtrack(S, left + 1, right)
+                S.pop()
+            
+            if right < left:
+                S.append(')')
+                backtrack(S, left, right + 1)
+                S.pop()
+        
+        backtrack([], 0, 0)
+
+        return ans
+
+# @lc code=end
+
