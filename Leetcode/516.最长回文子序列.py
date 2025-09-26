e92aa6ca3d2a44ec220d11164da8badf1d3c962f
LogCreative <logcreative@outlook.com>
2025-09-26 12:58:45 +0800

    pass 516

---
+++ Leetcode/516.最长回文子序列.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode.cn id=516 lang=python3
+#
+# [516] 最长回文子序列
+#
+
+# @lc code=start
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        n = len(s)
+        dp = [[0] * n for _ in range(n)]
+        # (i,j-1) (i,j)
+        # (i+1,j-1) (i+1,j) # 看的是下一行所以要反过来
+        for i in range(n-1,-1,-1):
+            dp[i][i] = 1
+            for j in range(i + 1, n):
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i+1][j-1] + 2
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+        return dp[0][n-1]
+        # 更难的做法是反转后求公共子序列，但不直观。
+# @lc code=end
+
