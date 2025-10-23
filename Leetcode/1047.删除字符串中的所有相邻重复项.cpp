87c2ce21e80eb86108f1913ecfc3bd27894768cb
LogCreative <logcreative@outlook.com>
2025-10-23 10:53:40 +0800

    pass 1047

---
+++ Leetcode/1047.删除字符串中的所有相邻重复项.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=1047 lang=cpp
+ *
+ * [1047] 删除字符串中的所有相邻重复项
+ */
+
+#include<iostream>
+#include<string>
+#include<stack>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string stk;
+        for (char c : s) {
+            if (!stk.empty() && stk.back() == c) {
+                stk.pop_back();
+            } else {
+                stk.push_back(c);
+            }
+        }
+        return stk;
+    }
+};
+// @lc code=end
+
